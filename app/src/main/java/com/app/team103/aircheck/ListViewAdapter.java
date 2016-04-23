2d01f695156a948b6c0a9a607e16afa89a824479
Inaki Abadia <i.abadiaosta@gmail.com>
2016-04-23 20:00:19 +0200

    Input data activities, custom listview adapter

---
+++ app/src/main/java/com/app/team103/aircheck/ListViewAdapter.java
@@ -0,0 +1,67 @@
+package com.app.team103.aircheck;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ListViewAdapter extends BaseAdapter implements ListAdapter {
+    private ArrayList<String> list = new ArrayList<>();
+    private Context context;
+
+
+
+    public ListViewAdapter(ArrayList<String> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+        return list.get(pos);
+    }
+
+    @Override
+    public long getItemId(int pos) {
+        return pos;
+        //just return 0 if your list items do not have an Id variable.
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.listview_item, null);
+        }
+
+        //Handle TextView and display string from your list
+        TextView listItemText = (TextView)view.findViewById(R.id.list_item_string);
+        listItemText.setText(list.get(position));
+
+        //Handle buttons and add onClickListeners
+        Button deleteBtn = (Button)view.findViewById(R.id.delete_btn);
+
+        deleteBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //do something
+                list.remove(position); //or some other task
+                notifyDataSetChanged();
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/com/app/team103/aircheck/MainActivity.java
+++ app/src/main/java/com/app/team103/aircheck/MainActivity.java
@@ -1,13 +1,66 @@
 package com.app.team103.aircheck;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static final String INPUT_TYPE = "INPUT_TYPE";
+    public static final String INPUT_SYMPTOM = "symptom";
+    public static final String INPUT_EVENT = "event";
+
+    private Button inputData_B, visualizeData_B, symptomsData_B, eventsData_B;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Acquire elements
+        inputData_B = (Button) findViewById(R.id.inputData_B);
+        visualizeData_B = (Button) findViewById(R.id.visualizeData_B);
+        symptomsData_B = (Button) findViewById(R.id.symptomsData_B);
+        eventsData_B = (Button) findViewById(R.id.eventsData_B);
+        // ClickListeners
+        inputData_B.setOnClickListener(this);
+        visualizeData_B.setOnClickListener(this);
+        symptomsData_B.setOnClickListener(this);
+        eventsData_B.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        visualizeData_B.setVisibility(View.VISIBLE);
+        symptomsData_B.setVisibility(View.INVISIBLE);
+        eventsData_B.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.inputData_B:
+                // Hide and show shit
+                visualizeData_B.setVisibility(View.INVISIBLE);
+                symptomsData_B.setVisibility(View.VISIBLE);
+                eventsData_B.setVisibility(View.VISIBLE);
+                break;
+            case R.id.visualizeData_B:
+                Intent visualizeDataAct = new Intent(this, VisualizeDataActivity.class);
+                startActivity(visualizeDataAct);
+                break;
+            case R.id.symptomsData_B:
+                Intent inputSymptomAct = new Intent(this, InputDataActivity.class);
+                inputSymptomAct.putExtra(INPUT_TYPE, INPUT_SYMPTOM);
+                startActivity(inputSymptomAct);
+                break;
+            case R.id.eventsData_B:
+                Intent inputEventAct = new Intent(this, InputDataActivity.class);
+                inputEventAct.putExtra(INPUT_TYPE, INPUT_EVENT);
+                startActivity(inputEventAct);
+                break;
+        }
     }
 }
